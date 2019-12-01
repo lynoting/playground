e8b2de14602cd2290c2b594d901ca1a6484afb03
lynoting <k.sakiko0627@gmail.com>
2019-12-01 16:50:47 +0900

    20191201

--- js/index.js
+++ js/index.js
@@ -1,33 +1,8 @@
 function init() {
-console.log("hooooooo");
 
-    const TEXTS = [
-    `フェデリコ・デ・ラ・フェが燃えさしのなかに手を突っ込むと、痛みのあまり悲しみは感じられなくなり、自らの肉が焦げる匂いしかしなくなった。古いスカーフで手を包み、クランデロから処方された緑色の軟膏を塗り込んだのち、彼は火によって治癒されたものをすべて書き留めた。
-
-1 痒み
-2 寝小便
-3 悲しみ
-
-　フェデリコ・デ・ラ・フェが唯一後悔したのは、十年前に火という方法を見つけ出せなかったことであった。
-— サルバトール・プラセンシア『紙の民』`,
-    `「チンメリア語の本はみんな中途で終わっています……」ウッツィ・トゥツィイはつぶやく、「なぜならかなたで続くからです……別の言語で、われわれが読んでいると思い込んでいるいろんな本のあらゆる言葉が最後にはそこへたどりつくことになる沈黙の言語で……」
-— イタロ・カルヴィーノ『冬の夜ひとりの旅人が』
-`,
-    `　その戦線は遠くから忍びより、みどりの領域は時間をかけて狭められていた。勘のするどいいきものたちが、からまりあった樹々をすりぬけてあらたな住処をさがす賭けに出ていた。重油の燃えたあとの煙がおおきな風のなかに入り混じり、喰うものも喰われるものも、走るのがわずかに遅くなった。気づいたときには、重機に出会ってかなしげになく生き物をたくさん見ていた。森でれんめんと編まれてきた生の記録を拭い去られてしまったにんげんたちもいた。そこには、あなたがちいさな子どものとき夜中のドキュメンタリ番組で見たのと同じ、あのどうしようもないかなしみがいっぱいにみちていた。でも、ことばとともにあるすべてのものたちに、かならずしもたしかな感官がそなわっているわけではない。わたしにはありあまるほどのことばを帯びていながら、それでいて決して痛みにつらぬかれないことをゆるされていた。ぼんやりしているうちに、なんの衝撃もないまま、わたしももはや森ではなくなっていた。ご覧のとおり。`,
-    `　わたしなんかがほんとうに森であったのかと、あなたが（あなたはまだいないけれど）疑うのも無理はない。でもたぶん、あなたにも森はいりまじっている。まずもってあなたのからだの大部分は水でできており、水はこれまで途方もない時間をかけて地球上をくまなく循環してきたのだから、あおあおとした樹林のはざまを流れぬけたことも、スコールとして大地をえぐり取ったこともあるに違いない。わたしはあなたほどからだにたくさんの水を含んではいないけれど、これと似たような理由で、かつてわたしは森だったと言っても嘘にはならない。もちろんあなたがひそかに思っているとおり、これは全くの詭弁。わたしたちの見知っているものをとても細かな要素へと還元し、その起源をどこまでもさかのぼれば、すくなくともかつて地球でなかった宇宙の塵にまで行きつくというのと同じで、こんなことに言及したところでむなしいばかりだ。けれども覚えているというのは嘘じゃない。森に育てられたわたしのからだのあちこちに、揺るぎない痕跡が埋め込まれている。あなたのからだも、きっと森の匂いを覚えているはずだと思う。`,
-    `As a part of the trend towards shorter forms, it was characteristic of Romantic writers to publish deliberate fragment, as opposed to accidental fragments such as the remains of Sappho’s poems or, if Macpherson were to be believed, Ossian’s. Gorthe’s Faust: Ein Fragment (1790) lent prestige to idea, and soon fragments of every genre were proliferating. Friedrich Schlegel’s Critical Fragments (1798) and Athenaeum Fragments (1800) were seminal contributions to Romantic theory and to the theory of the fragment itself. ‘Many of the works of the ancients have become fragments’, he writes. ‘Many Modern works are fragments as soon as they are written’ (AF 24, tr. Firchow). By these modern fragments, Schlegel means works that were ostensible complete and rounded out in their form but were really just a collection of imaginative moments.
-
-It is more honest to stop writing when a piece loses its inspiration and to name it a fragment. This Coleridge did in ‘Kubla Khan’: Or, A Vision in a Dream. A Fragment’ (written 1797) , and Byron did in the Giaour: A Fragment of a Turkish Tale (first edition 1813), though Coleridge offered a questionable excuse (he was interrupted by a visitor), and Byron kept adding fragments to subsequent editions. Several of Pushkin’s poems are labeled ‘fragment’ (otryvok). These and many other supposed ‘fragments’ seemed to attract by teasing, by rousing the reader’s imagination to fill in the gaps.
-
-— Romanticism: a Very Short Introduction, Michael Ferber`,
-    `本日もはるかむかしの秒音と
-零時丁度をお知らせします`,
-    `夜明けには全国的に晴れるでしょう 
-なにも飛行しない空でしょう`
-]
 $(document).ready(function () {
-    var reversedTexts = TEXTS.reverse();
-    reversedTexts.forEach(text => {
+    var reversedData = window.CardData.reverse();
+    reversedData.forEach(text => {
         var ranTopVal = Math.random() * 100 + 'px';
         var ranTransVal = 'translateX(' + Math.random() * 200 + 'px)'
         var ranWidthVal = 100 + (Math.random() * 100) + 'px'
