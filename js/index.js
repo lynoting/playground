5436073379590d9484baf4b05afbd5ea2f91f501
lynoting <k.sakiko0627@gmail.com>
2019-12-01 18:15:39 +0900

    20191201

--- js/index.js
+++ js/index.js
@@ -4,16 +4,15 @@ $(document).ready(function () {
     var reversedData = window.CardData.reverse();
     reversedData.forEach(text => {
         var ranTopVal = Math.random() * 100 + 'px';
-        var ranTransVal = 'translateX(' + Math.random() * 200 + 'px)'
-        var ranWidthVal = 200 + (Math.random() * 100) + 'px'
+        var ranTransVal = 'translateX(' + (Math.random() - 0.3) * 20 + 'vw)'
+        var ranWidthVal = 20 + (Math.random() * 5) + 'vw'
 
         $('.js-textcardContainer').append(`
-            <li 
+            <li
             class="pepTextCard"
             style="top:${ranTopVal}; 
             transform:${ranTransVal}; 
-            width:${ranWidthVal};
-            line-height: 1.5em";
+            width:${ranWidthVal};"
             >${text}</li>
         `)
     })
